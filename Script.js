8d5f4be22a98fbc54a43c53e54da13f6296c1a60
sramamoorthy2005-hash <sramamoorthy2005@gmail.com>
2026-07-16 22:45:18 +0530

    update

--- Script.js
+++ Script.js
@@ -1,7 +1,14 @@
 const burgerbutton =document.getElementById('burgerBtn');
 const navs = document.getElementById('navLists');
+const wrongBtn=document.getElementById('wrongBtn');
 
 burgerbutton.addEventListener('click',()=>{
    console.log('click');
     navs.classList.toggle('active');
+    if(wrongBtn.classList.contains('fa-bars')){
+        wrongBtn.classList.replace('fa-bars','fa-x');
+    }
+    else{
+        wrongBtn.classList.replace('fa-x','fa-bars');
+    }
 })
